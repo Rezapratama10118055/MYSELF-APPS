2a43b2b69793dc95602b281a29cc8342b5aad674
rezapratama10118055 <reza.10118055@mahasiswa.unikom.ac.id>
2021-05-23 11:13:41 +0700

    pertama
    Signed-off-by: rezapratama10118055 <reza.10118055@mahasiswa.unikom.ac.id>

---
+++ app/src/main/java/com/example/myselfapps/Daily/DailyAcitivtyini.java
@@ -0,0 +1,16 @@
+package com.example.myselfapps.Daily;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myselfapps.R;
+
+public class DailyAcitivtyini extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_acitivty);
+    }
+}
---
+++ app/src/main/java/com/example/myselfapps/ViewPageViewModel.java
@@ -0,0 +1,7 @@
+package com.example.myselfapps;
+
+import androidx.lifecycle.ViewModel;
+
+public class ViewPageViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
